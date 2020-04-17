d37dd73a4badde388c652f575da6dee1cd380e9f
Miguel Atencia <miguelatenciacanoles@hotmail.com>
2020-04-17 12:06:02 -0500

    Primer Commit,

---
+++ Curso.net/Curso.net/Models/SucursalCLS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Curso.net.Models
+{
+    public class SucursalCLS
+    {
+        [Display(Name ="ID Sucursal")]
+        public int iidsucursal { get; set; }
+
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+
+        [Display(Name = "Direccion")]
+        public string direccion { get; set; }
+
+        [Display(Name = "Telefono")]
+        public string telefono { get; set; }
+
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        public DateTime fechaapertura { get; set; }
+        public int bhabilitado { get; set; }
+    }
+}
---
+++ Curso.net/Curso.net/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Curso.net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Curso.net")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si ComVisible se establece en false, los componentes COM no verán los 
+// tipos de este ensamblado. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("dc8e9109-f2e8-40a9-a1d4-4ad0cfee8d41")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
