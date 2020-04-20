3ff6d13c20823f34826ddf3fe2e370bfaf141c8a
Miguel Atencia <miguelatenciacanoles@hotmail.com>
2020-04-20 15:10:11 -0500

    Se agrega y lista la  clase cliente

---
+++ CursoNetCore/Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CursoNetCore.Models;
+
+namespace CursoNetCore.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            List<ClienteCLS> ListaCliente = null;
+            using(var bd = new BDPasajeEntities())
+            {
+                ListaCliente = (from cliente in bd.Cliente
+                                where cliente.BHABILITADO == 1
+                                select new ClienteCLS
+                                {
+                                    iidcliente = cliente.IIDCLIENTE,
+                                    nombre = cliente.NOMBRE,
+                                    appaterno = cliente.APPATERNO,
+                                    apmaterno = cliente.APMATERNO,
+                                    email = cliente.EMAIL
+                                }
+                    ).ToList();
+            }
+            return View(ListaCliente);
+        }
+    }
+}
---
+++ CursoNetCore/Models/ClienteCLS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CursoNetCore.Models
+{
+    public class ClienteCLS
+    {
+        [Display(Name="ID Cliente")]
+        public int iidcliente { get; set; }
+
+        [Display(Name = "Nombre Cliente")]
+        public string nombre { get; set; }
+
+        [Display(Name = "Apellido Paterno")]
+        public string appaterno { get; set; }
+
+        [Display(Name = "Apellido Materno")]
+        public string apmaterno { get; set; }
+
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        public string direccion { get; set; }
+        public int iidsexo { get; set; }
+        public string telefonofijo { get; set; }
+        public string telefonocelular { get; set; }
+        public int bhabilitado { get; set; }
+        public int btieneusuario { get; set; }
+        public char tipousuario { get; set; }
+
+    }
+}
