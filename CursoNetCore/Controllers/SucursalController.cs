9cd173af6a4b0e05ec9fb8bcb4ee7c3e461940cf
matenciac14 <albertocanoles@hotmail.com>
2020-04-22 16:09:16 -0500

    Insertar datos en la tabla sucursal , manejo de control fecha en el formulario

--- CursoNetCore/Controllers/SucursalController.cs
+++ CursoNetCore/Controllers/SucursalController.cs
@@ -23,12 +23,42 @@ namespace CursoNetCore.Controllers
                                      iidsucursal = sucursal.IIDSUCURSAL,
                                      nombre = sucursal.NOMBRE,
                                      telefono = sucursal.TELEFONO,
+                                     direccion = sucursal.DIRECCION,
                                      email = sucursal.EMAIL
                                  }
                                  ).ToList();
 
             }
                 return View(ListaSucursal);
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(SucursalCLS oSucursalCLS)
+        {
+            if (!ModelState.IsValid) {
+                return View(oSucursalCLS);
+            }
+            else
+            {
+                using(var bd = new BDPasajeEntities())
+                {
+                    Sucursal oSucursal = new Sucursal();
+                    oSucursal.NOMBRE = oSucursalCLS.nombre;
+                    oSucursal.DIRECCION = oSucursalCLS.direccion;
+                    oSucursal.TELEFONO = oSucursalCLS.telefono;
+                    oSucursal.EMAIL = oSucursalCLS.email;
+                    oSucursal.FECHAAPERTURA = oSucursalCLS.fechaapertura;
+                    oSucursal.BHABILITADO = 1;
+                    bd.Sucursal.Add(oSucursal);
+                    bd.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- CursoNetCore/Models/SucursalCLS.cs
+++ CursoNetCore/Models/SucursalCLS.cs
@@ -12,18 +12,33 @@ namespace CursoNetCore.Models
         public int iidsucursal { get; set; }
 
         [Display(Name = "Nombre Sucursal")]
+        [StringLength(100,ErrorMessage ="Longitud maxima es 100")]
+        [Required]
         public string nombre { get; set; }
-        
+
+        [Display(Name = "Direccion Sucursal")]
+        [StringLength(200, ErrorMessage = "Longitud maxima es 200")]
+        [Required]
         public string direccion { get; set; }
 
         [Display(Name = "Telefono Sucursal")]
+        [StringLength(10, ErrorMessage = "Longitud maxima es 10")]
+        [Required]
         public string telefono { get; set; }
 
         [Display(Name = "Email Sucursal")]
+        [StringLength(100, ErrorMessage = "Longitud maxima es 100")]
+        [EmailAddress(ErrorMessage ="Ingrese un email Valido")]
+        [Required]
         public string email { get; set; }
 
-
+        [Display(Name = "Fecha Apertura")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+        [Required]
         public DateTime fechaapertura { get; set; }
+
+
         public int bhabilitado { get; set; }
     }
 }
